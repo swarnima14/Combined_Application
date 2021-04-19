6901f316f4e7a97d4b8dbd27d024430aa6070d25
swarnima14 <swarnima.tiwari14@gmail.com>
2021-04-19 20:21:26 +0530

    basic setup

--- app/src/main/java/com/app/combined/MainActivity.kt
+++ app/src/main/java/com/app/combined/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.app.combined
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BottomSheetBehavior.from(sheet).apply {
+            peekHeight = 200
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+
+            tvClassify.setOnClickListener {
+                Toast.makeText(this@MainActivity, "classify", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnCamera.setOnClickListener {
+            checkForPermission()
+        }
     }
+
+    private fun checkForPermission() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
+        }
+        else
+        openCamera()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 100 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            openCamera()
+        else
+            Toast.makeText(this, "Camera permission required", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun openCamera() {
+
+    }
+
+
 }
